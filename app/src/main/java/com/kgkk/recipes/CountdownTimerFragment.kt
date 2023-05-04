97ad2870e18031b3c54071a7f15600333acd4935
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-05-04 22:05:02 +0200

    Time picker for countdown

--- app/src/main/java/com/kgkk/recipes/CountdownTimerFragment.kt
+++ app/src/main/java/com/kgkk/recipes/CountdownTimerFragment.kt
@@ -6,11 +6,11 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class CountdownTimerFragment : Fragment() {
+class CountdownTimerFragment : Fragment(), TimePickerFragment.OnTimeSetListener {
 
     private lateinit var countdownTimer: CountDownTimer
     private lateinit var timerText: TextView
-    private var startingTime: Long = 60000
+    private var startingTime: Long = 0
     private var countdownTimeLeft: Long = startingTime // czas początkowy
 
     override fun onCreateView(
@@ -20,10 +20,27 @@ class CountdownTimerFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_countdown_timer, container, false)
         timerText = view.findViewById(R.id.timerText)
+
+        timerText.setOnClickListener {
+            val dialog = TimePickerFragment()
+            dialog.setListener(this)
+            dialog.show(parentFragmentManager, "countdown_time_picker")
+        }
+
         countdownTimer = createCountDownTimer()
         countdownButtonHandler(view)
         return view
     }
+
+    override fun onTimeSet(hourOfDay: Int, minute: Int, second: Int) {
+        val timeInMillis = (hourOfDay * 60 * 60 + minute * 60 + second) * 1000L
+        startingTime = timeInMillis
+        countdownTimeLeft = timeInMillis
+        countdownTimer = createCountDownTimer()
+        updateCountdownText()
+        this.view?.let { resetButtons(it) }
+    }
+
 
     private fun updateCountdownText() {
         val hours = countdownTimeLeft / (1000 * 60 * 60)
@@ -35,6 +52,9 @@ class CountdownTimerFragment : Fragment() {
     }
 
     fun createCountDownTimer(): CountDownTimer {
+        if (::countdownTimer.isInitialized){
+            countdownTimer.cancel()
+        }
         return object : CountDownTimer(countdownTimeLeft, 1000){
             override fun onTick(millisUntilFinished: Long) {
                 countdownTimeLeft = millisUntilFinished
@@ -45,6 +65,15 @@ class CountdownTimerFragment : Fragment() {
                 // sygnał dźwiękowy
             }
         }
+    }
+
+    private fun resetButtons(view: View){
+        val timerStartButton = view.findViewById<Button>(R.id.startButton)
+        val timerPauseButton = view.findViewById<Button>(R.id.pauseButton)
+        val timerResetButton = view.findViewById<Button>(R.id.resetButton)
+        timerStartButton.isEnabled = true
+        timerPauseButton.isEnabled = false
+        timerResetButton.isEnabled = false
     }
 
     private fun countdownButtonHandler(view: View) {
@@ -58,20 +87,16 @@ class CountdownTimerFragment : Fragment() {
             timerResetButton.isEnabled = true
         }
         timerPauseButton.setOnClickListener {
-            countdownTimer.cancel()
             countdownTimer = createCountDownTimer()
             timerStartButton.isEnabled = true
             timerPauseButton.isEnabled = false
             timerResetButton.isEnabled = true
         }
         timerResetButton.setOnClickListener {
-            countdownTimer.cancel()
             countdownTimeLeft = startingTime
             countdownTimer = createCountDownTimer()
             updateCountdownText()
-            timerStartButton.isEnabled = true
-            timerPauseButton.isEnabled = false
-            timerResetButton.isEnabled = false
+            resetButtons(view)
         }
     }
 }
