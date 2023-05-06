2519006f91a483eccb767dc50bb8084bdfd0b793
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-05-06 11:52:52 +0200

    Simple toolbar setup

--- app/src/main/java/com/kgkk/recipes/MainActivity.kt
+++ app/src/main/java/com/kgkk/recipes/MainActivity.kt
@@ -2,8 +2,12 @@ package com.kgkk.recipes
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentTransaction
 
 
@@ -11,6 +15,25 @@ class MainActivity : AppCompatActivity(), Listener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_info -> {
+                // Kod wykonywany po kliknięciu elementu Action_Info
+                Toast.makeText(this, "It's a recipe app!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun itemClicked(id: Int) {
